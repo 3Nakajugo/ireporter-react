456ee343013f39d675ea4c00abb6d9ec8e1f2e19
3Nakajugo <edinahmargarita@gmail.com>
2019-06-25 11:20:37 +0300

    chore(setup project): Setup an empty react project
    - install babel
    - create webpack congig file
    - create .gitnore file
    - create src directory
    
    [Starts #166799524]

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => { return <div>Hello Its edna</div>; };
+
+ReactDOM.render(<App />, document.querySelector('#root'));
